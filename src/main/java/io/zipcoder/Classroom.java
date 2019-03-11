051732e5e56931f3e25de252cd1f5cd6fa88b38f
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-11 19:24:03 -0400

    Started Classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,34 @@
 package io.zipcoder;
 
 public class Classroom {
+    private Student[] students;
+    private Integer maxNumberOfStudents;
+
+    public Classroom(Integer maxNumberOfStudents){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+
+    public Classroom(){
+        this.students = new Student[30];
+    }
+
+    public Integer getMaxNumberOfStudents(){
+        return maxNumberOfStudents;
+    }
+
+    public String getStudents(){
+        String output = "List of Students:\n";
+        for(Student s : students){
+            output += "\t" + s.getFirstName() + " " + s.getLastName() + "\n";
+        }
+        return output;
+    }
+
+    public Double getAverageExamScore(){
+        return null;
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,61 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+    @Test
+    public void maxStudentsConstructorTest(){
+        // given
+        Classroom classroom = new Classroom(40);
+        Integer expected = 40;
+        // when
+        Integer actual = classroom.getMaxNumberOfStudents();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMaxNumberOfStudentsTest(){
+        // given
+        Classroom classroom = new Classroom(40);
+        Integer expected = 40;
+        // when
+        Integer actual = classroom.getMaxNumberOfStudents();
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getStudentsTest(){
+        // given
+        Double[] examScores = {};
+        Student student1 = new Student("Alex", "Bozzini", examScores);
+        Student student2 = new Student("Wilhem", "Alcivar", examScores);
+        Student[] students = {student1, student2};
+        Classroom classroom = new Classroom(students);
+        // when
+        String output = classroom.getStudents();
+        // then
+        System.out.println(output);
+    }
+
+    @Test
+    public void getAverageExamScoreTest(){
+        // : Given
+        Double[] s1Scores = { 100.0, 150.0 };
+        Double[] s2Scores = { 225.0, 25.0 };
+
+        Student s1 = new Student("Alex", "Bozzini", s1Scores);
+        Student s2 = new Student("Wilhem", "Alcivar", s2Scores);
+
+        Student[] students = {s1, s2};
+        Classroom classroom = new Classroom(students);
+
+        // When
+        double output = classroom.getAverageExamScore();
+
+        // Then
+        System.out.println(output);
+    }
 }
