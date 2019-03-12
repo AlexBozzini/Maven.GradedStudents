1eb732dfeaf2635f73deaa9abc13b7f4917a4c0d
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-12 15:29:17 -0400

    Finished Classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,11 +1,13 @@
 package io.zipcoder;
 
-public class Classroom {
+import java.util.*;
+
+public class Classroom implements Comparator<Student> {
     private Student[] students;
     private Integer maxNumberOfStudents;
 
     public Classroom(Integer maxNumberOfStudents){
-        this.maxNumberOfStudents = maxNumberOfStudents;
+        this.students = new Student[maxNumberOfStudents];
     }
 
     public Classroom(Student[] students){
@@ -16,19 +18,76 @@ public class Classroom {
         this.students = new Student[30];
     }
 
-    public Integer getMaxNumberOfStudents(){
-        return maxNumberOfStudents;
-    }
-
     public String getStudents(){
-        String output = "List of Students:\n";
+        StringBuilder builder = new StringBuilder();
         for(Student s : students){
-            output += "\t" + s.getFirstName() + " " + s.getLastName() + "\n";
+            builder.append("========================\n");
+            try{
+                builder.append(s.toString());
+            }
+            catch (Exception e){
+                builder.append("No Students Enrolled\n");
+            }
+            builder.append("========================\n\n");
         }
-        return output;
+        return builder.toString();
     }
 
     public Double getAverageExamScore(){
-        return null;
+        Double sum = 0.0;
+        for (Student s : students){
+            sum += s.getAverageExamScore();
+        }
+        return sum / students.length;
+    }
+
+    public void addStudent(Student student){
+        List<Student> studentsList = new ArrayList<>(Arrays.asList(students));
+        studentsList.add(student);
+        studentsList.remove(null);
+        students = studentsList.toArray(new Student[0]);
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        List<Student> studentList = new ArrayList<>(Arrays.asList(students));
+        for (Student s : studentList){
+            if (s.getFirstName().equalsIgnoreCase(firstName) && s.getLastName().equalsIgnoreCase(lastName)){
+                studentList.remove(s);
+                break;
+            }
+        }
+        studentList.sort(this);
+        students = studentList.toArray(new Student[0]);
+    }
+
+    public ArrayList<Student> getStudentByScore(){
+        ArrayList<Student> studentList = new ArrayList<>(Arrays.asList(students));
+        studentList.sort(this);
+        return studentList;
+    }
+
+    public String getGradeBook(){
+        Map<String, ArrayList<Student>> gradeBook = new TreeMap<>();
+        ArrayList<Student> sortedStudentList = getStudentByScore();
+        Integer binSize = sortedStudentList.size()/5;
+        gradeBook.put("A", new ArrayList<>(sortedStudentList.subList(0, binSize)));
+        gradeBook.put("B", new ArrayList<>(sortedStudentList.subList(binSize, binSize * 2)));
+        gradeBook.put("C", new ArrayList<>(sortedStudentList.subList(binSize * 2, binSize * 3)));
+        gradeBook.put("D", new ArrayList<>(sortedStudentList.subList(binSize * 3, binSize * 4)));
+        gradeBook.put("F", new ArrayList<>(sortedStudentList.subList(binSize * 4, binSize * 5)));
+
+        StringBuilder builder = new StringBuilder();
+        for(Map.Entry entry : gradeBook.entrySet()){
+            builder.append("" + entry.getKey() + " Students: \n\n\n" + entry.getValue().toString() + "\n");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public int compare(Student s1, Student s2) {
+        if (s2.getAverageExamScore().compareTo(s1.getAverageExamScore()) == 0){
+            return s1.getLastName().compareTo(s2.getLastName());
+        }
+        return s2.getAverageExamScore().compareTo(s1.getAverageExamScore());
     }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -3,29 +3,9 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class ClassroomTest {
-    @Test
-    public void maxStudentsConstructorTest(){
-        // given
-        Classroom classroom = new Classroom(40);
-        Integer expected = 40;
-        // when
-        Integer actual = classroom.getMaxNumberOfStudents();
-        // then
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void getMaxNumberOfStudentsTest(){
-        // given
-        Classroom classroom = new Classroom(40);
-        Integer expected = 40;
-        // when
-        Integer actual = classroom.getMaxNumberOfStudents();
-        // then
-        Assert.assertEquals(expected, actual);
-    }
-
     @Test
     public void getStudentsTest(){
         // given
@@ -57,5 +37,95 @@ public class ClassroomTest {
 
         // Then
         System.out.println(output);
+    }
+
+    @Test
+    public void addStudentTest(){
+        // : Given
+        int maxNumberOfStudents = 1;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student s1 = new Student("Alex", "Bozzini", examScores);
+
+        // When
+        String preEnrollment = classroom.getStudents();
+        classroom.addStudent(s1);
+        String postEnrollment = classroom.getStudents();
+
+        // Then
+        System.out.println(preEnrollment);
+        System.out.println(postEnrollment);
+    }
+
+    @Test
+    public void removeStudentTest(){
+        // given
+        Double[] s1Scores = {100.0, 150.0};
+        Double[] s2Scores = {50.0, 100.0};
+
+        Student s1 = new Student("Alex", "Bozzini", s1Scores);
+        Student s2 = new Student("Wilhem", "Alcivar", s2Scores);
+
+        Student[] students = new Student[]{s1, s2};
+        Classroom classroom = new Classroom(students);
+        String preRemoval = classroom.getStudents();
+        // when
+        classroom.removeStudent("Wilhem", "Alcivar");
+        String postRemoval = classroom.getStudents();
+        // then
+        System.out.println(preRemoval);
+        System.out.println(postRemoval);
+    }
+
+    @Test
+    public void getStudentByScore(){
+        // given
+        Double[] s1Scores = {100.0, 150.0};
+        Double[] s2Scores = {50.0, 100.0};
+        Double[] s3Scores = {80.0, 110.0};
+
+        Student s1 = new Student("Alex", "Bozzini", s1Scores);
+        Student s2 = new Student("Wilhen", "Alcivar", s2Scores);
+        Student s3 = new Student("Andrew", "Telepak", s3Scores);
+
+        Student[] students = new Student[]{s1, s2, s3};
+        Classroom classroom = new Classroom(students);
+        // when
+        String sortedStudents = classroom.getStudentByScore().toString();
+        // then
+        System.out.println(sortedStudents);
+    }
+
+    @Test
+    public void getGradeBookTest(){
+        // given
+        Double[] s1Scores = {10.0, 10.0};
+        Double[] s2Scores = {20.0, 20.0};
+        Double[] s3Scores = {30.0, 30.0};
+        Double[] s4Scores = {40.0, 40.0};
+        Double[] s5Scores = {50.0, 50.0};
+        Double[] s6Scores = {60.0, 60.0};
+        Double[] s7Scores = {70.0, 70.0};
+        Double[] s8Scores = {80.0, 80.0};
+        Double[] s9Scores = {90.0, 90.0};
+        Double[] s10Scores = {100.0, 100.0};
+
+        Student s1 = new Student("Alex", "Bozzini", s1Scores);
+        Student s2 = new Student("Wilhem", "Alcivar", s2Scores);
+        Student s3 = new Student("Andrew", "Telepak", s3Scores);
+        Student s4 = new Student("Ross", "Eckbold", s4Scores);
+        Student s5 = new Student("Gigi", "Kaloustian", s5Scores);
+        Student s6 = new Student("Kath", "Singh", s6Scores);
+        Student s7 = new Student("Sofia", "Demopolos", s7Scores);
+        Student s8 = new Student("Jenna", "Verdieck", s8Scores);
+        Student s9 = new Student("Tara", "Lustair", s9Scores);
+        Student s10 = new Student("Sara", "Rocco", s10Scores);
+
+        Student[] students = new Student[]{s1, s2, s3, s4, s5, s6, s7, s8, s9, s10};
+        Classroom classroom = new Classroom(students);
+        // when
+        String gradeBook = classroom.getGradeBook();
+        // then
+        System.out.println(gradeBook);
     }
 }
